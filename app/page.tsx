1eb58f1925d3cc5d1ef8067de31b73a4f8b4dfb5
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-12-11 22:40:47 +0900

    Headerコンポーネントを実装し、TypeScript Propsによる状態管理に対応

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
+import Header from "@/components/Header";
+
 export default function Home() {
+
+    // 　仮のログインデータ（開発中）
+    const isUserLoggedIn = true;//trueでログイン状態、falseで未ログイン状態
+    const currentUserName = "HibikiOno"
+
     return (
-        <main className="flex min-h-screen flex-col items-center p-12 text-center font-sans ">
+        <main className="min-h-screen font-sans ">
+            
+            {/* ヘッダーコンポーネントを引数渡して呼び出し */}
+            <Header isLoggedIn={isUserLoggedIn} userName={currentUserName} />
 
-            <h1 className="text-5xl font-bold mb-5">
-                Tabi-Log
-            </h1>
+            {/* コンテンツ */}
+            <div className="flex flex-col items-center p-12 text-center">
+                <h1 className="text-4xl font-extrabold mb-8 text-gray-800">
+                    Tabi-Log
+                </h1>
 
-            <p className="text-l">
-                旅の思い出づくりのお手伝い・・・
-            </p>
-
+                <p className="text-2xl font-semibold text-indigo-600">
+                    旅の思い出づくりのお手伝い・・・
+                </p>
+            </div>
         </main>
     )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,35 @@
-export default function Header() {
+interface HeaderProps {
+    // ユーザのログイン状態(boolean)
+    isLoggedIn: boolean;
+    // ユーザ名（String)
+    userName?: string;
+}
+
+
+export default function Header({ isLoggedIn, userName }: HeaderProps) {
+
+    // ログイン状態に応じて、ボタンの中身を切り替える
+    const buttonContents = isLoggedIn ? (
+        //ログイン済の場合
+        <p className="text-black py-2 px-4">ようこそ、{userName}さん</p>
+    ):(
+        //未ログインの場合
+        <button className="bg-indigo-500 cursor-pointer text-white  py-2 px-4 rounded-lg hover:bg-indigo-600 transition duration-150">
+            ログイン
+        </button>
+    )
+
     return (
-        <header className="w-full boder-b border-gray-200 p-4">
+        <header className="w-full border-b bg-stone-50 border-gray-200 p-4">
             <nav className="flex items-center justify-between max-w-7xl mx-auto">
                 {/* サービス名 */}
                 <div className="text-2xl font-bold text-indigo-600">
                     Tabi-Log
                 </div>
 
-                {/* ログインボタン  */}
-                <button className="bg-indigo-500 text-while py-2 px-4 rounded-lg hover:bg-indigo-600 transition duration-150">
-                    ログイン
-                </button>
+                {/* ログインボタン */}
+                {buttonContents}
+
             </nav>
         </header>
     )
