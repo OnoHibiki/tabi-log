3fc7f3e8bbdbddc1945adfd3b68f4d9f048546d3
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-12-12 23:43:17 +0900

    lambda用Pythonバックエンド準備

---
+++ backend/lambda_function.py
@@ -0,0 +1,20 @@
+import json
+
+def lambda_handler(event, context):
+    
+    print("Pythonバックエンドが呼び出されました")
+
+    response_body = {
+        "message" : "Hello from Python Backend!",
+        "status"  : "success"
+    }
+
+    # API Gateway用のレスポンス
+    return {
+        "statusCode" : 200,
+        "headers" : {
+            "Content-Type": "application/json" ,
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps(response_body, ensure_ascii=False)
+    }
