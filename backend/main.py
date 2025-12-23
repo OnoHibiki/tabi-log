ff97bfa2ded37034493ff4f1e7f9a53b00da8f29
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-12-23 19:26:42 +0900

    画像保存用のstaticフォルダ作成と、DBスキーマ変更

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,13 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from database import get_db_connection 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
